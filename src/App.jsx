8ba5ceb874e389e9301ebe1428e52f68bcb320c6
emman-likha <bulaonjohnemm4nuel.21@gmail.com>
2025-02-07 20:06:49 +0800

    Create and design the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,18 @@ function App() {
   return (
     < >
    <header className=''>
-  <nav className="bg-gray-600 text-2xl text-cyan-400 p-2 flex items-center">
-  <img className="mt-4 mx-auto w-32 sm:w-48 md:w-64 lg:w-80" src="/src/assets/e.png" alt="Image description" />
-    <ul className="ml-auto flex space-x-4">
+  <nav className="bg-gray-600 text-2xl text-cyan-400 p-2 flex items-center ">
+  <img className=" w-12 " src="/src/assets/e.png" alt="Image description" />
+    <ul className="">
       <li>
-        <a href="#welcome-section" className="hover:text-white">Click Me</a>
+        <a href="#welcome-section" className="hover:text-cyan-200 border-cyan-400 ">HOME</a>
+        <select className="appearance-none ">
+          <option><a href='#about-me'>ABOUT  ME</a></option>
+          <option>EDUCATION</option>
+          <option>GOAL</option>
+        
+        </select>
+        <a href="#welcome-section" className="hover:text-white mx-4">PROJECT</a>
       </li>
     </ul>
   </nav>
@@ -23,6 +30,9 @@ function App() {
 
 <main>
   <section id="welcome-section">
+    
+  </section>
+  <section id="about-me">
     
   </section>
 </main>
