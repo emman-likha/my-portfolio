b268ec435be7edf1505d4acbc0d39b00a9dfbb06
bulaonjohnemm4nuel.21@gmail.com <bulaonjohnemm4nuel.21@gmail.com>
2025-02-10 22:29:05 +0800

    appjsx: improve UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
   return (
     < >
    <html class="scroll-smooth h-screen">
-   <header className="flex justify-center ">
+  <section>
+     <header className="flex justify-center ">
    <div className="fixed flex items-center  mt-3 bg-gray-950 opacity-30 hover:opacity-100  border-3 rounded-full font-[Quicksand] hover:border-x-5 hover:shadow-md hover:shadow-cyan-300/50 shadow-lg  hover:border-cyan-300 ease-in-out ">
   <nav className=" text-xl px-6 py-2  text-cyan-400 flex items-center hover:mx-3 transition-all duration-700 hover:drop-shadow-lg ">
   {/*<img className=" w-12 ml-10" src="/src/assets/e.png" alt="Image description" />
@@ -39,22 +40,22 @@ function App() {
   
   <section class="w-full h-screen bg-gray-900  flex justify-center items-center flex-col" id="welcome-section ">
     <h1 class="mb-20 -mt-10 text-cyan-400 flex flex-col items-center text-4xl gap-3 font-[Quicksand]">WELCOME TO MY <span class="text-9xl font-[Quicksand] font-black">PORTFOLIO</span></h1>
-    <div class="flex flex-row gap-30 bg-cyan-950 opacity">
+    <div class="flex flex-row gap-30 bg-cyan-950 justify-center">
 
       <div class=" w-3xl h-96 rounded-2xl text-amber-50">
-       <div class="pl-3">
+       
         <h1 class="text-5xl">I am a Programmer</h1>
         <h1 class="text-5xl">I am a Creative Artist</h1>
         <h1 class="text-5xl">I am a Computer Engineer</h1>
         <h1 class="text-5xl">I am a Content Creator</h1>
         <h1 class="text-5xl">I am a Video Editor</h1>
-       </div>
+       
       </div>
 
       <div class="bg-amber-100 w-3xl h-96 rounded-3xl">
 
       </div>
-      <div clas=""></div>
+      
     </div>
     
   </section>
@@ -85,6 +86,7 @@ function App() {
   <footer>
     <a href="https://github.com/emman-likha" target="_blank">Github</a>
   </footer>
+  </section>
    </html>
 
   
